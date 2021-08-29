78c6b87fdf11de96b431e7421d404a948cefc0e1
RFNoC <rfnoc@rfnocspur11154t.lan>
2021-08-29 14:27:30 -0700

    test

--- Wes Bos JS/x.js
+++
@@ -1,8 +0,0 @@
-const { dogName = 'snickers' } = { dogName: undefined }
-console.log(dogName) // what will it be? 'snickers'!
-const { dogName1 = 'snickers' } = { dogName1: null }
-console.log(dogName1) // what will it be? null!
-const { dogName2 = 'snickers' } = { dogName2: false }
-console.log(dogName2) // what will it be? false!
-const { dogName3 = 'snickers' } = { dogName3: 0 }
-console.log(dogName3) // what will it be? 0!
