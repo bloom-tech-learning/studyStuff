c9e771d0aee19390df5d41a295adf81374dde8c0
Rongjun GENG <bigsur_xigeng@weijies-mac-pro.lan>
2021-09-15 20:26:51 -0700

    web 1.3 project 3 assign

--- Module_13/Project3/assigment/README.md
+++ Module_13/Project3/assigment/README.md
@@ -1,49 +1,15 @@
-# FIFA Challenge - JavaScript-Foundations
+# Constructors, Prototypes and _this_
 
-## Advanced JavaScript
-
-## Array Methods and Callbacks
-*Note*: Please add the LIVE server extension to your visual studio code and use it for this project. https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer - you will not be able to view this data in the browser or see your console logs without it. 
+## JavaScript Fundamentals
 
 ## Objectives
 
-- Array Methods - demonstrate ability to use `.forEach()` `.map()` `.reduce()` `.filter()` and describe their use cases
-- Callbacks - demonstrate the proper use a of a callback function by passing a function to another function as a parameter
-  
+- explain the four principles of the 'this' keyword and demonstrate each explanation with an example of their uses
+- describe, and use prototypes, constructor functions the new keyword, and pseudo-classical inheritance to build objects
+
 ## Introduction
 
-In this challenge, you will work with a data set summarizing every World Cup game from 1930 to 2014. As web developers, you will often work with JSON data to interact with servers, summarize data, get images to your page, and more. It is crucial that you cultivate a thorough understanding of data structures and methods early in your career.
-
-The data set you are given (loaded in at the top of `index.js`) contains a report for every game played at every stage in the FIFA World Cup tournament since 1930. Each report is an object with many keys, including `Year`,`Stage`, `Stadium`,  `Home Team Name`,`Home Team Goals`, `Away Team Name`, and `Away Team Goals`. There is a plethora of other data in this set as well, but we won't spend time working with it. You can read more about the data [here](https://www.kaggle.com/abecklas/fifa-world-cup) or look at the sample object below if you are interested!
-
-Your goal today will be to manipulate the data using array methods and callbacks to create interesting statistics.
-
-### Sample Data
-
-```js
-  {
-      "Year": 1930,
-      "Datetime": "13 Jul 1930 - 15:00",
-      "Stage": "Group 1",
-      "Stadium": "Pocitos",
-      "City": "Montevideo",
-      "Home Team Name": "France",
-      "Home Team Goals": 4,
-      "Away Team Goals": 1,
-      "Away Team Name": "Mexico",
-      "Win conditions": "",
-      "Attendance": 4444,
-      "Half-time Home Goals": 3,
-      "Half-time Away Goals": 0,
-      "Referee": "LOMBARDI Domingo (URU)",
-      "Assistant 1": "CRISTOPHE Henry (BEL)",
-      "Assistant 2": "REGO Gilberto (BRA)",
-      "RoundID": 201,
-      "MatchID": 1096,
-      "Home Team Initials": "FRA",
-      "Away Team Initials": "MEX"
-    },
-```
+In order to complete these tasks you will need your newly aquired knowledge of constructor functions, prototypes, and the `this` keyword.
 
 ## Instructions
 
@@ -59,11 +25,13 @@ Using VSCode and Command Line:
 5. next type `npm run test` in your terminal
 6. Complete your work making regular commits, once you have all your tests passing and you are ready to submit your work please see canvas for instructions on how to submit
 
-### Task 2: Minimum Viable Product
 
-Find the file `index.js` and complete the tasks until all of your tests pass.
+### Task 2 - MVP
 
-You may want to use the [World Cup summary data](https://www.foxsports.com/soccer/fifa-world-cup/history) on Fox Sports to check your math as you go.
+Find the file `index.js` and complete tasks 1, 2 and 3 until all of your tests pass.
+There is an additional task 4 which requires written explanations and has no tests.
+
+If you run into trouble while coding, fight the good fight for 20 minutes and then get on the help channel. __Remember to formulate your help request in a professional manner__ - like you would at the job - by including error messages, screenshots, and any other pertinent information about the problem, as well as what things you have attempted already while trying to solve it.
 
 ### Testing & Debugging
 
@@ -76,42 +44,9 @@ Inside of your second terminal type `npm start`
 You will be running your tests in one terminal and debugging in the other. As you work on your code you should make use of `console.log` to check your progress and debug.
 ![alt text](assets/tests_debug_terminal_final.png "your terminal should look like this")
 
-### Task 3: Stretch Goals
+### Task 3 - Stretch 
 
-After you have completed the requirements, create a new file called `stretch.js` and practice with Array methods more.
-
-See if you can complete one or more of the following challenges:
-
-- [ ] Create a function that takes country initials as a parameter and returns their total number of World Cup appearances.
-- [ ] Account for ties in your 'finals' data set
-- [ ] Create a function that takes country initials as a parameter and determines how many goals that country has scored in World Cup games since 1930.
-- [ ] Use `.map()` to format country names into `<h1>` HTML headers.
-
-## FAQ
-
-**Where is my data?**
-
-Your data lives in a file called `fifa.js`. In order to work with and use your data in the `index.js` file, we used the import function of JavaScript. The first line in your file should be `import { fifaData } from "fifa.js"`, which will save all of the data into an object called fifaData. You can confirm that your data has been imported correctly by running `console.log(fifaData)` in your `index.js` file.
-
-**What about ties?**
-
-In some cases, when the outcome of a game was decided by penalty kicks, the home team and away team will have the same number of goals scored. You can ignore ties for the purpose of these tasks, but you can also look to the "Win conditions" key  and splice out the winning team as a stretch goal. (See example below)
-
-```
-Win conditions": "France win on penalties (3 - 4)",
-````
-
-## Resources
-
-📚 [Examples of .map, .reduce, and .filter](https://itnext.io/15-useful-javascript-examples-of-map-reduce-and-filter-74cbbb5e0a1f)
-
-🤷‍♀️ [What the Heck is a Callback?](https://codeburst.io/javascript-what-the-heck-is-a-callback-aba4da2deced)
-
-🤝[Working with JSON in JavaScript](https://www.ma-no.org/en/programming/javascript/working-with-json-in-javascript)
-
-⚽️ [World Cup Summary Data to Check Work](https://www.foxsports.com/soccer/fifa-world-cup/history)
-
-👀 [List of FIFA World Cup finals on Wikipedia](https://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals)
+There are stretch goals found throughout `index.js`. Do not start work on these until you have finished MVP. 
 
 ## Submission format
 
--- Module_13/Project3/assigment/index.js
+++ Module_13/Project3/assigment/index.js
@@ -1,218 +1,135 @@
-const { fifaData } = require('./fifa.js')
-
-// ⚽️ M  V P ⚽️ //
-
-/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 /*
-const finals2014 = fifaData.filter(function(item){
-    return item.Year === 2014 && item.Stage == 'Final';
-})
+  EXAMPLE TASK:
+    - Write an Airplane constructor that initializes `name` from an argument.
+    - All airplanes built with Airplane should initialize with an `isFlying` of false.
+    - Give airplanes the ability to `.takeOff()` and `.land()`:
+        + If a plane takes off, its `isFlying` property is set to true.
+        + If a plane lands, its `isFlying` property is set to false.
 */
 
-// function finnals2014 = (data) => data.filter (item => item.Year === 2014 && item.Stage === 'Final');
+// EXAMPLE SOLUTION CODE:
+function Airplane(name) {
+  this.name = name;
+  this.isFlying = false;
+}
+Airplane.prototype.takeOff = function () {
+  this.isFlying = true;
+};
+Airplane.prototype.land = function () {
+  this.isFlying = false;
+};
 
-// console.log('task 1: ',finnals2014(fifaData));
-    
-const finals2014 = fifaData.filter(item => item.Year === 2014 && item.Stage === 'Final');
-
-console.log(finals2014);
-
-//(a) Home Team name for 2014 world cup final
-
-console.log(finals2014[0]['Home Team Name']);
-
-//(b) Away Team name for 2014 world cup final
-
-console.log(finals2014[0]['Away Team Name']);
-
-//(c) Home Team goals for 2014 world cup final
-
-console.log(finals2014[0]['Home Team Goals']);
-
-//(d) Away Team goals for 2014 world cup final
-
-console.log(finals2014[0]['Away Team Goals']);
-
-//(e) Winner of 2014 world cup final */
-
-console.log(finals2014[0]['Win conditions']);
-
-/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
-Use getFinals to do the following:
-1. Receive data as a parameter
-2. Return an array of objects with the data of the teams that made it to the final stage
-
-hint - you should be looking at the stage key inside of the objects
+/*
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
 */
 
 /*
-function getFinals(data) {
-    // code here 
-    return data.filter(function(item){
-          return item.Stage === 'Final';
-    });
- }
- */
+  TASK 1
+    - Write a Person Constructor that initializes `name` and `age` from arguments.
+    - All instances of Person should initialize with an empty `stomach` array.
+    - Give instances of Person the ability to `.eat("someFood")`:
+        + When eating an edible, it should be pushed into the `stomach`.
+        + The `eat` method should have no effect if there are 10 items in the `stomach`.
+    - Give instances of Person the ability to `.poop()`:
+        + When an instance poops, its `stomach` should empty.
+    - Give instances of Person a method `.toString()`:
+        + It should return a string with `name` and `age`. Example: "Mary, 50"
+*/
 
- /*
- function getFinals(data){
-     return data.filter( item => item.Stage === 'Final');
- }
- */
+function Person(name, age) {
+  this.name = name;
+  this.age = age;  
+  this.stomach = [];
+}
+Person.prototype.eat = function (someFood) {
+  if (this.stomach.length < 10 ) {
+    this.stomach.push(someFood) 
+  } else {
+    console.log(`no effect`);
+  }    
+  return this.stomach;
+};
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
+Person.prototype.toString = function (name, age) {
+  return `${this.name}, ${this.age}`;
+};
+
+console.log('task 1: ', new Person('Rongjun GENG', 50));
+
+/*
+  TASK 2
+    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    - All instances built with Car:
+        + should initialize with an `tank` at 0
+        + should initialize with an `odometer` at 0
+    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+        + Should cause the `odometer` to go up.
+        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+*/
+
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon =milesPerGallon;
+  this.tank = 0;
+  this.odometer =0;  
+}
+Car.prototype.fill = function (gallons)  {
+  return this.tank += gallons;
+};
  
- // const getFinals = (data => data.filter( item => item.Stage === 'Final'));
- 
-const getFinals = (data) => data.filter( item => item.Stage === 'Final');
- 
-console.log(getFinals(fifaData));
- 
-
-/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Use the higher-order function called getYears to do the following: 
-1. Receive an array
-2. Receive a callback function getFinals from task 2 
-3. Return an array called years containing all of the years in the getFinals data set
-*/
+const myCar = new Car('Prius', 84);
+myCar.fill(20); 
 
 /*
-function getYears(data, getFinals_cb) {
-    // code here 
-    const yearsArr = [];
-    const finalsDate = getFinals_cb(data);
-    for (let i = 0; i < finalsDate.length; i++) {
-        yearsArr.push(finalsDate[i].Year);
-    }      
-    return yearsArr;   
+  TASK 3
+    - Write a Baby constructor subclassing Person.
+    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+    - Besides the methods on Person.prototype, babies have the ability to `.play()`:
+        + Should return a string "Playing with x", x being the favorite toy.
+*/
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype.play = function () {
+  return `"Playing with ${this.favoriteToy}", ${this.favoriteToy} being the favorite toy`;
+};
+Baby.prototype.poop = function () {
+  Person.prototype.poop(this); 
+};
+Baby.prototype.eat = function (someFood) {
+  Person.prototype.eat(this, someFood);
+}
+
+// const myBaby = Baby(new Person('Lucy', 4), 'Lego trains');
+
+/* 
+  TASK 4
+  In your own words explain the four principles for the "this" keyword below:
+  1. Window binding - no context for the 'this';
+  2. Implicit binding - objects with methods when the function is invoked, look to the left of the dot that's what 'this' refers to.
+  3. Explicit binding - explicitly pass in as an argument of what want 'this' to refer to.
+  4. New binding - which is used with constructor functions.
 */
 
-/*
-function getYears(data, getFinals_cb) {
-    const yearsArr = [];
-    getFinals_cb(data).forEach(function(item){
-        yearsArr.push(item.Year);        
-    });
-    return yearsArr;
-}
-*/
+///////// END OF CHALLENGE /////////
 
-/*
-function getYears(data, getFinals_cb) {
-    const yearsArr = [];
-    getFinals_cb(data).forEach(item => yearsArr.push(item.Year));        
-    return yearsArr;
-}
-*/
-
-const getYears = (data, getFinals_cb) => getFinals_cb(data).map(item => item.Year); 
-      
-console.log('task3:', getYears(fifaData, getFinals));
-
-/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Use the higher-order function getWinners to do the following:  
-1. Receives an array
-2. Receives the callback function getFinals from task 2 
-3. Determines the winner (home or away) of each `finals` game. 
-4. Returns the names of all winning countries in an array called `winners` */ 
-
-/*
-function getWinners(data, getFinals_cb) {
-    
-    return getFinals_cb(data).map(item => item['Home Team Goals'] > item['Away Team Goals'] ? item['Home Team Name'] : item['Away Team Name']);      
-}
-*/
-
-const getWinners = (data, getFinals_cb) => getFinals_cb(data).map(item => (item['Home Team Goals'] > item['Away Team Goals']) ? item['Home Team Name'] : item['Away Team Name']);
-
-console.log(getWinners(fifaData, getFinals));
-
-/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
-Use the higher-order function getWinnersByYear to do the following:
-1. Receive an array
-2. Receive a callback function getYears from task 3
-3. Receive a callback function getWinners from task 4
-4. Return an array of strings that say "In {year}, {country} won the world cup!" 
-
-hint: the strings returned need to exactly match the string in step 4.
- */
-
-/*
-function getWinnersByYear(data, getYears_cb, getWinners_cb) {
-    const yearsArr = getYears_cb(data, getFinals);
-    const winnersArr = getWinners_cb(data, getFinals);
-
-    return winnersArr.map((item, index) => `In ${yearsArr[index]}, ${item} won the world cup!`);
-}
-*/
-
-const getWinnersByYear = (data, getYears_cb, getWinners_cb) => getWinners_cb(data, getFinals).map((item, index) => `In ${getYears_cb(data, getFinals)[index]}, ${item} won the world cup!`);
-
-console.log('Task 5: ', getWinnersByYear(fifaData, getYears, getWinners));
-
-/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Use the higher order function getAverageGoals to do the following: 
- 1. Receive the callback function getFinals from task 2 ensure you pass in the data as an argument
- 2. Return the the average number of the total home team goals and away team goals scored per match and round to the second decimal place. 
- 
- (Hint: use .reduce and do this in 2 steps) 
- 
- Example of invocation: getAverageGoals(getFinals(fifaData));
-*/
-/*
-function getAverageGoals(data) {
-    const averageGoals = data.reduce((acc, item) => acc + item['Home Team Goals'] + item['Away Team Goals'], 0);
-    return (averageGoals / data.length).toFixed(2);
-}
-*/
-
-const getAverageGoals = (data => ((data.reduce((acc, item) => acc + item['Home Team Goals'] + item['Away Team Goals'], 0)) /data.length).toFixed(2));
- 
-console.log('Task 6: ', getAverageGoals(fifaData));
-
-/// 🥅 STRETCH 🥅 ///
-
-/* 💪💪💪💪💪 Stretch 1: 💪💪💪💪💪 
-Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
-
-Hint: Investigate your data to find "team initials"!
-Hint: use `.reduce` */
-
-function getCountryWins(data) {
-
-}
-
-/* 💪💪💪💪💪 Stretch 2: 💪💪💪💪💪 
-Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
-
-function getGoals(/* code here */) {
-
-    /* code here */
-
-}
-
-/* 💪💪💪💪💪 Stretch 3: 💪💪💪💪💪
-Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
-
-function badDefense(/* code here */) {
-
-    /* code here */
-
-}
-
-/* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
-
-/* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
+/* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
 function foo(){
-    console.log('its working');
-    return 'bar';
+  console.log('its working!');
+  return 'bar';
 }
 foo();
 module.exports = {
-    foo,
-    getFinals,
-    getYears,
-    getWinners,
-    getWinnersByYear,
-    getAverageGoals
+  foo,
+  Person, 
+  Car,
+  Baby
 }
